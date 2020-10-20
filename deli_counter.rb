97a547ba5a5f7795e56a2b13f585dd9bc1a7685c
Jacob W Jones <jacobwadejones76@gmail.com>
2020-10-20 22:40:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ end
 
 def take_a_number(line_array, person)
   line_array << person
-  puts "Welcom #{person}, you are number #{line_array.length} in line!"
+  puts "Welcom #{person}, you are number #{line_array.length} in line."
 
