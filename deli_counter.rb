865de84f5be6f4fa1c7bccfe6bb9fd94f8794894
Jacob W Jones <jacobwadejones76@gmail.com>
2020-10-20 22:42:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(line_array)
   if line_array.length == 0
     puts "The line is currently empty."
   else 
-    current_line = "Your position is line:"
+    current_line = "The line is currently:"
     line_array.each_with_index do |person, index|
       current_line.concat(" #{index + 1}. #{person}")
     end
